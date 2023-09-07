cb706422af6c086db9750606f7316238131d933a
Oksana Korobko <oksana.korobko.fd@gmail.com>
2023-09-08 00:25:50 +0300

    Start to work with usersList

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import './App.css';
 import binary from './test'
-import Questions from './Components/Questions';
-import Result from './Components/Result';
+// import Questions from './Components/Questions';
+// import Result from './Components/Result';
 import { useState } from 'react';
+import Students from './Components/Students';
 
 function App() {
-  const [step, setStep ] = useState(0);
-  const question = binary[step];
+  
+  // const [step, setStep ] = useState(0);
+  // const [correct, setCorrect] = useState(0);
+  // const question = binary[step];
 
-  const onClickVariant = (index)=> {
-    console.log (step, index);
-    setStep(prevState => prevState+1)
-  }
+  // const onClickVariant = (index)=> {
+  //   (index===question.correct)&& setCorrect(prevState => prevState+1);
+  //   setStep(prevState => prevState+1)
+
+  // }
 
   return (
     <div className="App">
-      <header><h1>Двійкове кодування</h1></header>  
-      <Questions question = {question} onClickVariant = {onClickVariant}/>
-      <Result/>     
+      {/* Test */}
+      {/* <header><h1>Двійкове кодування</h1></header>  
+      {
+        step !== binary.length ? <Questions step = {step} question = {question} onClickVariant = {onClickVariant}/> :<Result correct = {correct}/>  
+      } */}
+      {/* SudentsList */}
+      <Students />
+         
     </div>
   );
 }
